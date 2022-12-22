04ea8919a41c4170a4619cf2f62771fb3be4e10e
moassamo <moassermo@gmail.com>
2022-12-22 09:28:37 -0600

    password length function

--- script.js
+++ script.js
@@ -10,5 +10,25 @@ var uppercaseCheck;
 var numberCheck;
 var specialCheck;
 
+//Function used to determine the length of the password
+function determineLength(){
+  passwordLength = prompt("Choose how many characters long you'd like your password to be (between 8-128 characters): ");
+
+    if (passwordLength<8){
+      alert("Password length must be a number between 8-128 characters");
+      determineLength();
+    }else if (passwordLength>128){
+      alert("Password length must be a number between 8-128 characters");
+      determineLength();
+    }else if (isNaN(passwordLength)){
+      alert("Password length must be a number between 8-128 characters");
+      determineLength();
+    }else{
+    alert("The next three screens will ask you what types of characters you would like to be included in your password.\nIf you choose 'No' for all, your password will only contain lowercase letters.");
+    }
+    return passwordLength;
+}
+
+
 
 
